5bf21ae05d5f11724f71e83f847f6c9d9dd5a772
GiovannePDS7 <giovanne3282@gmail.com>
2025-06-16 13:27:53 -0300

    rota de cadastro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -57,8 +57,11 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (nickname == undefined) {
+        res.status(400).send("Seu nickname está undefined!");
+    }else if (empresa == undefined) {
+        res.status(400).send("Sua empresa está undefined!");
     }else {
-
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
         usuarioModel.cadastrar(nome, email, senha, empresa, nickname)
             .then(
